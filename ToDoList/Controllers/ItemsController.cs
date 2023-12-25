7d279673b8f406cb75917988a1138f22a504c14e
SimphiweMbokazi <simphiwem380@gmail.com>
2023-12-25 18:02:47 +0200

    Refactor views and add new DbContext class

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,28 +1,65 @@
+// using Microsoft.AspNetCore.Mvc;
+// using ToDoList.Models;
+// using System.Collections.Generic;
+
+// namespace ToDoList.Controllers
+// {
+//   public class ItemsController : Controller
+//   {
+
+//     [HttpGet("/categories/{categoryId}/items/new")]
+//     public ActionResult New(int categoryId)
+//     {
+//        Category category = Category.Find(categoryId);
+//        return View(category);
+//     }
+
+//     [HttpGet("/categories/{categoryId}/items/{itemId}")]
+//     public ActionResult Show(int categoryId, int itemId)
+//     {
+//       Item item = Item.Find(itemId);
+//       Category category = Category.Find(categoryId);
+//       Dictionary<string, object> model = new Dictionary<string, object>();
+//       model.Add("item", item);
+//       model.Add("category", category);
+//       return View(model);
+//     }
+//   }
+// }
+
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
-using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
-  public class ItemsController : Controller
-  {
-
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
+    public class ItemsController : Controller
     {
-       Category category = Category.Find(categoryId);
-       return View(category);
-    }
+        private readonly ToDoListContext _db;
 
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
-    {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
-      return View(model);
+        public ItemsController(ToDoListContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Item> model = _db.Items.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Item item)
+        {
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
-  }
 }
