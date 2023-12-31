7b67bf835e17fb7308b17d49be6572a49ed3c3ae
SimphiweMbokazi <simphiwem380@gmail.com>
2023-12-31 15:35:46 +0200

    Refactor ItemsController and Index view

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,36 +1,9 @@
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-// using System.Collections.Generic;
-
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
-
-//     [HttpGet("/categories/{categoryId}/items/new")]
-//     public ActionResult New(int categoryId)
-//     {
-//        Category category = Category.Find(categoryId);
-//        return View(category);
-//     }
-
-//     [HttpGet("/categories/{categoryId}/items/{itemId}")]
-//     public ActionResult Show(int categoryId, int itemId)
-//     {
-//       Item item = Item.Find(itemId);
-//       Category category = Category.Find(categoryId);
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       model.Add("item", item);
-//       model.Add("category", category);
-//       return View(model);
-//     }
-//   }
-// }
 
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
@@ -45,7 +18,7 @@ namespace ToDoList.Controllers
 
         public ActionResult Index()
         {
-            List<Item> model = _db.Items.ToList();
+            List<Item> model = _db.Items.Include(item => item.Category).ToList();
             return View(model);
         }
 
